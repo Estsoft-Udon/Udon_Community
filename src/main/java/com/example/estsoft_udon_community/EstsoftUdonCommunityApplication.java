a5d5e9e40e9d8db29411307f8650219c448f962a
SongJwans <yhh5916@naver.com>
2024-10-28 11:06:37 +0900

    송주환: initial commit

---
+++ src/main/java/com/example/estsoft_udon_community/EstsoftUdonCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.estsoft_udon_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstsoftUdonCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstsoftUdonCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estsoft_udon_community/EstsoftUdonCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estsoft_udon_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstsoftUdonCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
