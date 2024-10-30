02580f765f155a6b91f1f86184c77ea3256d3166
jingeon <ephigh1397@naver.com>
2024-10-30 14:04:11 +0900

    Feat: add Comments

---
+++ src/main/java/com/example/estsoft_udon_community/entity/Comments.java
@@ -0,0 +1,48 @@
+package com.example.estsoft_udon_community.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = false)
+    private LocalDateTime deletedAt;
+
+    @Column(nullable = false)
+    private Boolean isDeleted = Boolean.FALSE;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Articles articles;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    public Comments(Articles articles, Users users) {
+        this.articles = articles;
+        this.users = users;
+    }
+}
