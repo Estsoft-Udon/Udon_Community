a33f9f9f4d8df557ef5c5fc9e899a419acb3c789
SongJwans <yhh5916@naver.com>
2024-10-30 13:53:54 +0900

    Users Entity 추가

---
+++ src/main/java/com/example/estsoft_udon_community/entity/Grade.java
@@ -0,0 +1,19 @@
+package com.example.estsoft_udon_community.entity;
+
+public enum Grade {
+    UDON("우동"),
+    UDON_FRIEND("우동 친구"),
+    UDON_SHERIFF("우동보안관"),
+    UDON_MASTER("우동마스터"),
+    UDON_ADMIN("우동관리자");
+
+    private final String displayName;
+
+    Grade(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
