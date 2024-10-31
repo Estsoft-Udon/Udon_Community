a32157117b90cd2fab4a8eaa897c54b03fb161db
jingeon <ephigh1397@naver.com>
2024-10-31 14:26:26 +0900

    Feat: Comments CRUD API 구현
    저장 조회 수정 삭제
    #52 #53 #54 #55

---
+++ src/main/java/com/example/estsoft_udon_community/service/CommentsService.java
@@ -0,0 +1,62 @@
+package com.example.estsoft_udon_community.service;
+
+import com.example.estsoft_udon_community.entity.Articles;
+import com.example.estsoft_udon_community.entity.Comments;
+import com.example.estsoft_udon_community.entity.dto.CommentsRequest;
+import com.example.estsoft_udon_community.repository.CommentsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentsService {
+    private final CommentsRepository commentsRepository;
+    private final ArticlesRepository articlesRepository;
+
+    public CommentsService(CommentsRepository commentsRepository, ArticlesRepository articlesRepository) {
+        this.commentsRepository = commentsRepository;
+        this.articlesRepository = articlesRepository;
+    }
+
+    // 댓글 추가
+    public Comments saveComment(Long articleId, CommentsRequest request) {
+        Articles articles = articlesRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("not found article id : " + articleId));
+
+        return commentsRepository.save(new Comments(articles, request.getContent()));
+    }
+
+    // 댓글 목록 조회
+    public Comments findComment(Long commentsId) {
+        Optional<Comments> optionalComments = commentsRepository.findById(commentsId);
+        return optionalComments.orElse(new Comments());
+    }
+
+    public List<Comments> findCommentsByArticleId(Long articleId) {
+        articlesRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("not found article id : " + articleId));
+
+        return commentsRepository.findByArticleId(articleId);
+    }
+
+    // 댓글 수정
+    public Comments update(Long commentId, CommentsRequest request) {
+        Comments comments = findComment(commentId);
+        comments.updateCommentsContent(request.getContent());
+
+        return commentsRepository.save(comments);
+    }
+
+    // 댓글 삭제
+    public void deleteBy(Long commentsId) {
+        commentsRepository.deleteById(commentsId);
+    }
+}
+
+/*
+댓글 추가	POST	/api/articles/{articleId}/comments
+댓글목록조회	GET	/api/articles/{articleId}/comments
+댓글 수정	PUT	/api/comments/{commentId}
+댓글 삭제	DELETE	/api/comments/{commentId}
+ */
