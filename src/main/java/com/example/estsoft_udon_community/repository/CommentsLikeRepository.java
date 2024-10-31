4e58506605a83f008d421c67785f0726b3efdd42
jingeon <ephigh1397@naver.com>
2024-10-31 14:28:49 +0900

    Feat: Add ArticlesLikeRepository, CommentsLikeRepository

---
+++ src/main/java/com/example/estsoft_udon_community/repository/CommentsLikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.estsoft_udon_community.repository;
+
+import com.example.estsoft_udon_community.entity.CommentsLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentsLikeRepository extends JpaRepository<CommentsLike, Long> {
+}
