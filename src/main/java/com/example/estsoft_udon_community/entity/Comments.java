cdba5ddfcdee93443cda6c546c9eed7e3e56d756
jingeon <ephigh1397@naver.com>
2024-11-02 01:48:07 +0900

    Fix: Comments Entity 컬럼명 오류 수정

--- src/main/java/com/example/estsoft_udon_community/entity/Comments.java
+++ src/main/java/com/example/estsoft_udon_community/entity/Comments.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class Comments {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Long id;
 
     @ManyToOne
