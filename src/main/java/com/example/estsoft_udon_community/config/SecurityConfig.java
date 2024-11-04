4b103985224b53ce3b516184253f59a81848afb5
jingeon <ephigh1397@naver.com>
2024-11-04 15:23:00 +0900

    feat: Security making

--- src/main/java/com/example/estsoft_udon_community/config/SecurityConfig.java
+++ src/main/java/com/example/estsoft_udon_community/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,5 +26,22 @@ public class SecurityConfig {
                                 .anyRequest().permitAll()
                 ).csrf(custom -> custom.disable())
                 .build();
+    }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http.authorizeHttpRequests(
+//                        custom -> custom.requestMatchers("/login").permitAll()
+//                                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll() // 정적 리소스 허용
+//                                .anyRequest().authenticated())          // 그 외에는 인증 필요
+//                .formLogin(custom -> custom.loginPage("/login")         // 폼 기반 로그인 설정
+//                        .defaultSuccessUrl("/articles"))                // 인증 성공 시 여기로 리다이렉트
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .build();
+//    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
