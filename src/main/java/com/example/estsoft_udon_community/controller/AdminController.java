b1a147825b7b754777ce26bcf53dc341ddfed265
Sang Won Lee <freerusya@naver.com>
2024-11-04 16:39:29 +0900

    feat: 관리자 회원 관리 리스트
    관리자 회원 리스트 가져오기

--- src/main/java/com/example/estsoft_udon_community/controller/AdminController.java
+++ src/main/java/com/example/estsoft_udon_community/controller/AdminController.java
@@ -7,10 +7,9 @@ import com.example.estsoft_udon_community.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -23,5 +22,12 @@ public class AdminController {
     public ResponseEntity<UsersResponse> login(@RequestBody UsersRequest request) {
         Users admin = adminService.adminLogin(request.getLoginId(), request.getPassword());
         return ResponseEntity.status(HttpStatus.CREATED).body(new UsersResponse(admin));
+    }
+
+    // 관리자 회원 관리 리스트
+    @GetMapping("/users")
+    public ResponseEntity<List<UsersResponse>> getAllUsers() {
+        List<UsersResponse> userList = adminService.getAllUsers();
+        return ResponseEntity.ok(userList);
     }
 }
--- src/main/java/com/example/estsoft_udon_community/service/AdminService.java
+++ src/main/java/com/example/estsoft_udon_community/service/AdminService.java
@@ -1,10 +1,13 @@
 package com.example.estsoft_udon_community.service;
 
+import com.example.estsoft_udon_community.dto.response.UsersResponse;
 import com.example.estsoft_udon_community.entity.Users;
 import com.example.estsoft_udon_community.enums.Grade;
 import com.example.estsoft_udon_community.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,13 @@ public class AdminService {
             throw new IllegalArgumentException("관리자가 아님");
         }
         return adminUser;
+    }
+
+    // 회원 관리 리스트
+    public List<UsersResponse> getAllUsers() {
+        List<Users> users = usersRepository.findAll();
+        return users.stream()
+                .map(UsersResponse::new)
+                .toList();
     }
 }
