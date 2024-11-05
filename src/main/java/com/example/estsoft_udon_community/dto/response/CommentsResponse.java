d95e9c4db1de1229a0d08bd303a0ce4dbd0f1893
jingeon <ephigh1397@naver.com>
2024-11-06 01:05:15 +0900

    modify: CommentsResponse 필드 추가
    Users user, String nickName 추가

--- src/main/java/com/example/estsoft_udon_community/dto/response/CommentsResponse.java
+++ src/main/java/com/example/estsoft_udon_community/dto/response/CommentsResponse.java
@@ -1,6 +1,7 @@
 package com.example.estsoft_udon_community.dto.response;
 
 import com.example.estsoft_udon_community.entity.Comments;
+import com.example.estsoft_udon_community.entity.Users;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentsResponse {
+    private Users user;
+    private String nickName;
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public CommentsResponse(Comments comments) {
+        this.user = comments.getUsers();
+        this.nickName = comments.getUsers().getNickname();
         this.content = comments.getContent();
         this.createdAt = comments.getCreatedAt();
         this.updatedAt = comments.getUpdatedAt();
