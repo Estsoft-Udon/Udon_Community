623a8b2533a186bf215a49fd25b161d6b257d90c
SongJwans <yhh5916@naver.com>
2024-11-05 16:03:02 +0900

    feat: 로그인 시 header 창 변경

--- src/main/java/com/example/estsoft_udon_community/config/SecurityConfig.java
+++ src/main/java/com/example/estsoft_udon_community/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.estsoft_udon_community.config;
 
+import com.example.estsoft_udon_community.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +8,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+
     @Bean
     public WebSecurityCustomizer ignore() {
         return WebSecurity -> WebSecurity.ignoring()
@@ -24,7 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(
                         custom -> custom.requestMatchers("/api/**").permitAll()
-                                .requestMatchers("/login", "/signup", "/find_id", "/find_pw").permitAll()
+                                .requestMatchers("/", "/login", "/signup", "/find_id", "/find_pw").permitAll()
                                 .anyRequest().hasAnyRole("UDON", "UDON_FRIEND", "UDON_SHERIFF", "UDON_MASTER", "UDON_ADMIN")
                 )
                 .formLogin(custom -> custom.loginPage("/login"))
--- src/main/java/com/example/estsoft_udon_community/controller/BoardController.java
+++ src/main/java/com/example/estsoft_udon_community/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.example.estsoft_udon_community.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.estsoft_udon_community.entity.Location;
@@ -8,13 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
     private final LocationService locationService;
-
-    public BoardController(LocationService locationService) {
-        this.locationService = locationService;
-    }
 
     @GetMapping("/board_list")
     public String boardList() {
@@ -25,8 +24,10 @@ public class BoardController {
     public String boardDetail() {
         return "board/board_detail";
     }
+
     @GetMapping("/board_edit")
     public String boardEdit(Model model) {
+
         // 상위 지역 목록을 가져와서 모델에 추가
         List<String> upperLocations = locationService.getDistinctUpperLocations();
         model.addAttribute("upperLocations", upperLocations);
