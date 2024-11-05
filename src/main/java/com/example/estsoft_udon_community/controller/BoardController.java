1668cb86b06d9120394fa304e4b4567bf667ff6a
goldsonge <ty7968@gmail.com>
2024-11-05 11:35:49 +0900

    feat : 지역 선택 셀렉트 데이터 삽입

--- src/main/java/com/example/estsoft_udon_community/controller/BoardController.java
+++ src/main/java/com/example/estsoft_udon_community/controller/BoardController.java
@@ -2,19 +2,41 @@ package com.example.estsoft_udon_community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.example.estsoft_udon_community.entity.Location;
+import com.example.estsoft_udon_community.service.LocationService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 @Controller
 public class BoardController {
+    private final LocationService locationService;
+
+    public BoardController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
     @GetMapping("/board_list")
     public String boardList() {
         return "board/board_list";
     }
+
     @GetMapping("/board_detail")
     public String boardDetail() {
         return "board/board_detail";
     }
     @GetMapping("/board_edit")
-    public String boardEdit() {
+    public String boardEdit(Model model) {
+        // 상위 지역 목록을 가져와서 모델에 추가
+        List<String> upperLocations = locationService.getDistinctUpperLocations();
+        model.addAttribute("upperLocations", upperLocations);
+
+        // 첫 번째 상위 지역에 대한 하위 지역 목록을 초기화하여 모델에 추가
+        if (!upperLocations.isEmpty()) {
+            String firstUpperLocation = upperLocations.get(0);
+            List<Location> lowerLocations = locationService.getLowerLocation(firstUpperLocation);
+            model.addAttribute("locations", lowerLocations);
+        }
         return "board/board_edit";
     }
 }
