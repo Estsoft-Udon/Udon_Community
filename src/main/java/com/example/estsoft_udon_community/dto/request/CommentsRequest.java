3905c4255a26328b34926d3de1e46302944da69e
jingeon <ephigh1397@naver.com>
2024-11-06 01:04:48 +0900

    modify: CommentsRequest 생성자 추가
    Comments 를 파라미터로 CommentsRequest를 생성하는 생성자 추가

--- src/main/java/com/example/estsoft_udon_community/dto/request/CommentsRequest.java
+++ src/main/java/com/example/estsoft_udon_community/dto/request/CommentsRequest.java
@@ -4,11 +4,22 @@ import com.example.estsoft_udon_community.entity.Comments;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentsRequest {
     private long user_id;
     private String content;
+    private LocalDateTime created_at;
+
+    public CommentsRequest(Comments comments) {
+        this.user_id = comments.getUsers().getId();
+        this.content = comments.getContent();
+        this.created_at = comments.getCreatedAt();
+    }
 }
