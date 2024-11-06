f407342c6b2da7007f9344535486f540b6772dc9
dejeong <ahjeong921@gmail.com>
2024-11-06 15:22:30 +0900

    refactor : 이벤트 view 컨트롤러 분리

--- src/main/java/com/example/estsoft_udon_community/controller/admin/AdminEventViewController.java
+++ src/main/java/com/example/estsoft_udon_community/controller/admin/AdminEventViewController.java
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin")
-public class AdminViewController {
+@RequestMapping("/admin/event")
+public class AdminEventViewController {
     private final AdminEventService adminEventService;
 
     @Autowired
-    public AdminViewController(AdminEventService adminEventService) {
+    public AdminEventViewController(AdminEventService adminEventService) {
         this.adminEventService = adminEventService;
     }
 
     // 이벤트 목록 조회 (기본적으로 전체 목록)
-    @GetMapping("/event/event_list")
+    @GetMapping("/event_list")
     public String eventList(@RequestParam(value = "approved", required = false) Boolean approved, Model model) {
         List<EventResponse> events;
 
@@ -39,7 +39,7 @@ public class AdminViewController {
 
 
     // 이벤트 승인 여부 수정
-    @GetMapping("/event/event_edit/{id}")
+    @GetMapping("/event_edit/{id}")
     public String eventEdit(@PathVariable Long id, Model model) {
         EventResponse event = adminEventService.getEventById(id);
         model.addAttribute("event", event);
@@ -47,14 +47,14 @@ public class AdminViewController {
     }
 
     // 이벤트 승인 처리
-    @PostMapping("/event/{id}/approve")
+    @PostMapping("/{id}/approve")
     public String approveEvent(@PathVariable Long id) {
         adminEventService.approveEvent(id);
         return "redirect:/admin/event/event_edit/{id}";
     }
 
     // 이벤트 취소 처리 (승인 취소)
-    @PostMapping("/event/{id}/cancel")
+    @PostMapping("/{id}/cancel")
     public String cancelEvent(@PathVariable Long id) {
         adminEventService.cancelEvent(id);
         return "redirect:/admin/event/event_edit/{id}";
