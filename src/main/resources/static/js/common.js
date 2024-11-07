c849478a5ee89e43e2e45e2d59812a750b5b1212
Sang Won Lee <freerusya@naver.com>
2024-11-07 15:51:51 +0900

    feat: 회원가입 시 비밀번호 정규식, 일치검사 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -51,6 +51,7 @@ function initializeSelectBox(selectors) {
     });
 }
 
+// 아이디 중복 확인
 function checkId() {
     const loginId = document.getElementById('loginId').value.trim();
 
@@ -92,5 +93,43 @@ function checkId() {
             console.error('아이디 중복 확인 오류:', error);
         });
 }
+
+// 비밀번호 정규식 검증
+function validatePassword() {
+    const password = document.getElementById('password').value.trim();
+    const passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    const passwordMessage = document.getElementById('passwordMessage');
+
+    if (!passwordPattern.test(password)) {
+        passwordMessage.textContent = '최소 8자 이상, 영문자, 숫자, 특수문자가 포함.';
+        passwordMessage.style.color = 'red';
+    } else {
+        passwordMessage.textContent = '';
+    }
+}
+// 비밀번호 확인
+function checkPasswordMatch() {
+    const password = document.getElementById('password').value.trim();
+    const confirmPassword = document.getElementById('confirmPassword').value.trim();
+    const messageElement = document.getElementById('passwordMatchMessage');
+    if (!password || !confirmPassword) {
+        messageElement.textContent = '';
+        return;
+    }
+    if (password !== confirmPassword) {
+        messageElement.textContent = '비밀번호가 일치하지 않습니다.';
+        messageElement.style.color = 'red';
+    } else {
+        messageElement.textContent = '비밀번호가 일치합니다.';
+        messageElement.style.color = 'green';
+    }
+}
+// 비밀번호 입력 시 실시간 정규식 검사
+document.getElementById('password').addEventListener('input', function() {
+    validatePassword(); // 비밀번호 정규식 검증
+    checkPasswordMatch(); // 비밀번호 일치 여부 확인
+});
+// 비밀번호 확인 입력 시 실시간 비밀번호 일치 여부 확인
+document.getElementById('confirmPassword').addEventListener('input', checkPasswordMatch);
 
 initializeSelectBox('#upperLocation, #locationId');
