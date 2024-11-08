cf15cafe72b22ce1602295db66f1c6a9e18f5a22
dejeong <ahjeong921@gmail.com>
2024-11-08 14:44:06 +0900

    feat : 관리자 캘린더 승인 요청 목록 이벤트 타입 displayName 추가

--- src/main/java/com/example/estsoft_udon_community/enums/EventType.java
+++ src/main/java/com/example/estsoft_udon_community/enums/EventType.java
@@ -1,6 +1,16 @@
 package com.example.estsoft_udon_community.enums;
 
 public enum EventType {
-    FESTIVAL,
-    GATHERING
+    FESTIVAL("축제"),
+    GATHERING("소모임");
+
+    private final String displayName;
+
+    EventType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
