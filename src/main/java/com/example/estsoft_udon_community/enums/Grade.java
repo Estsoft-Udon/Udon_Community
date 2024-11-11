cb00061d238fa95a0e4b7986aa5bd816a1d8d5d3
SongJwans <yhh5916@naver.com>
2024-11-11 15:00:39 +0900

    refactor : UDON_MASTER 삭제

--- src/main/java/com/example/estsoft_udon_community/enums/Grade.java
+++ src/main/java/com/example/estsoft_udon_community/enums/Grade.java
@@ -1,12 +1,9 @@
 package com.example.estsoft_udon_community.enums;
 
-import lombok.Getter;
-
 public enum Grade {
     UDON("우동"),
     UDON_FRIEND("우동 친구"),
     UDON_SHERIFF("우동보안관"),
-    UDON_MASTER("우동마스터"),
     UDON_ADMIN("우동관리자");
 
     private final String displayName;
